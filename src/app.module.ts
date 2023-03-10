1e5f1b8ed37c870bf36ccd4647bd9293839d2f06
Paulo Chaves <opaulochaves@gmail.com>
2023-03-10 15:45:00 -0300

    add knex

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { KnexModule } from 'nest-knexjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    KnexModule.forRoot({
+      config: {
+        client: 'pg',
+        debug: true,
+        connection: process.env.DATABASE_URL,
+        pool: { min: 0, max: 7, idleTimeoutMillis: 300_000 },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
